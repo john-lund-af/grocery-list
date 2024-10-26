46ad558d73ea2b3c3d0eac44e02d902088015230
John Lundgren <john.lundgren.lexicon@gmail.com>
2024-10-26 10:31:36 +0200

    Added palette colors.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      textColor: {
+        skin: {
+          base: 'var(--color-text-base)',
+          inverted: 'var(--color-text-inverted)',
+          accent: 'var(--color-text-accent)',
+          action: 'var(--color-text-action)'
+        }
+      },
+      backgroundColor: {
+        skin: {
+          fill: 'var(--color-fill)',
+          'button-accent': 'var(--color-text-accent)'
+        }
+      }
+    },
   },
   plugins: [],
 }
