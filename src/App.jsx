b608917112f8faf0cc98fac7de0923ffb04d382f
John Lundgren <john.lundgren.lexicon@gmail.com>
2024-10-28 07:17:06 +0100

    Added Header comp.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Footer from './components/footer/Footer';
 import FloatingActionButton from './components/FloatingActionButton/FloatingActionButton';
+import Header from './components/header/Header';
 
 function App() {
 
   return (
     <>
+      <Header>Grocery<br/>List</Header>
       <FloatingActionButton />
       <Footer />
     </>
