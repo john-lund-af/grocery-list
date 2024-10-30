e131ed2bc407cfc7e0b621057c85a9c7b2ff8e76
John Lundgren <john.lundgren.lexicon@gmail.com>
2024-10-30 09:05:58 +0100

    Wrapping App component with the grocery context.

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,18 @@ import './App.css'
 import Footer from './components/footer/Footer';
 import FloatingActionButton from './components/FloatingActionButton/FloatingActionButton';
 import Header from './components/header/Header';
+import GroceryContextProvider from './context/GroceryContextProvider';
+import GroceryList from './components/groceryList/GroceryList';
 
 function App() {
 
   return (
-    <>
+    <GroceryContextProvider>
       <Header>Grocery<br/>List</Header>
+      <GroceryList />
       <FloatingActionButton />
       <Footer />
-    </>
+    </GroceryContextProvider>
   )
 }
 
