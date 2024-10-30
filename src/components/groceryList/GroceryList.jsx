2cfa560f1422f6ef44e88065a49f1c25cf2d88eb
John Lundgren <john.lundgren.lexicon@gmail.com>
2024-10-30 09:07:41 +0100

    Changes in Grocery List to use the GroceryContext.

--- src/components/groceryList/GroceryList.jsx
+++ src/components/groceryList/GroceryList.jsx
@@ -1,10 +1,31 @@
 import PropTypes from 'prop-types';
 import GroceryItem from '../groceryItem/GroceryItem';
+import {useContext, useEffect} from 'react';
+import GroceryContext from '../../context/GroceryContext';
+import axios from 'axios';
 
-const GroceryList = ({groceries}) => {
+const GroceryList = () => {
+  const {state, dispatch} = useContext(GroceryContext);
+
+  useEffect(() => {
+    async function getAsyncData() {
+      try{
+        dispatch({type: 'SET_LOADING_STATUS'})
+        const result = await axios.get('http://localhost:3001/groceries');
+        dispatch({type: 'GROCERIES_LOADED', groceries: result.data});
+      } catch(err){
+        console.error(`Error fetching groceries ${err.name} ${err.message}`)
+      }
+    }
+    getAsyncData();
+  }, [dispatch])
+
+  if(state.loading)
+    return <h2 className='relative top-44 text-lg'>Loading....</h2>
+
   return (
-    <ul className='relative top-44 z-0'>
-      {groceries.map(grocery => <GroceryItem key={grocery.id} grocery={grocery} />)}
+    <ul className='relative top-44 pb-20 z-0'>
+      {state.groceries.map(grocery => <GroceryItem key={grocery.id} grocery={grocery} />)}
     </ul>
   )
 }
