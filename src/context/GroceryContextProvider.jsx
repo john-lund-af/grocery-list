e7e2e8ebb9a0f98b8f2c5de152396c2261959c6d
John Lundgren <john.lundgren.lexicon@gmail.com>
2024-11-02 18:07:45 +0100

    Added case for errors in reducer hook.

--- src/context/GroceryContextProvider.jsx
+++ src/context/GroceryContextProvider.jsx
@@ -5,14 +5,16 @@ import {useReducer} from 'react';
 function reducer(state, action) {
   switch(action.type){
     case 'GROCERIES_LOADED':
-      return {...state, loading: false, groceries: action.groceries};
+      return {...state, loading: false, groceries: action.groceries, error: null};
     case 'SET_LOADING_STATUS':
-      return {...state, loading: true};
+      return {...state, loading: true, error: null};
     case 'UPDATE_GROCERY':
     { 
       const groceriesUpdated = state.groceries.map(grocery => grocery.id === action.grocery.id ? action.grocery : grocery)
-      return {...state, groceries: groceriesUpdated}; 
-    }
+      return {...state, groceries: groceriesUpdated, error: null}; 
+    };
+    case 'SET_ERROR':
+      return {...state, error: action.error, loading: false}
     default:
       throw new Error(`case failure. type: ${action.type}`);
   }
@@ -20,7 +22,8 @@ function reducer(state, action) {
 
 const initialState = {
   groceries: [],
-  loading: true
+  loading: false,
+  error: null
 }
 
 const GroceryContextProvider = ({children}) => {
