1c8ea33d87a065ef54944e5c5b2ce3e3953979e4
John Lundgren <john.lundgren.lexicon@gmail.com>
2024-11-02 18:08:42 +0100

    Usage of Error component.

--- src/components/groceryList/GroceryList.jsx
+++ src/components/groceryList/GroceryList.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import GroceryItem from '../groceryItem/GroceryItem';
+import Error from '../error/Error';
 import {useContext, useEffect} from 'react';
 import GroceryContext from '../../context/GroceryContext';
 import axios from 'axios';
@@ -14,11 +15,15 @@ const GroceryList = () => {
         const result = await axios.get('http://localhost:3001/groceries');
         dispatch({type: 'GROCERIES_LOADED', groceries: result.data});
       } catch(err){
+        dispatch({type: 'SET_ERROR', error: err})
         console.error(`Error fetching groceries ${err.name} ${err.message}`)
       }
     }
     getAsyncData();
   }, [dispatch])
+
+  if(state.error)
+    return <Error error={state.error} />
 
   if(state.loading)
     return <h2 className='relative top-44 text-lg'>Loading....</h2>
