36c74888c7f736a112a5492937ff0443c12927f1
John Lundgren <john.lundgren.lexicon@gmail.com>
2024-11-04 15:36:56 +0100

    Impl Edit grocery item functionality

--- src/components/groceryItem/GroceryItem.jsx
+++ src/components/groceryItem/GroceryItem.jsx
@@ -2,28 +2,47 @@ import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPenToSquare, faTrashCan } from '@fortawesome/free-solid-svg-icons'
 import axios from 'axios';
-import {useContext} from 'react';
+import {useContext, useState} from 'react';
 import GroceryContext from '../../context/GroceryContext';
 
 const GroceryItem = ({grocery}) => {
   const {dispatch, URI} = useContext(GroceryContext);
+  const [groceryName, setGroceryName] = useState(grocery.name);
+  const [editMode, setEditMode] = useState(false);
 
-  async function handleRemove(groceryId) {
+  async function handleRemove() {
     try {
-      await axios.delete(`${URI}/${groceryId}`);
-      dispatch({type: 'GROCERY_REMOVED', groceryId});
+      await axios.delete(`${URI}/${grocery.id}`);
+      dispatch({type: 'GROCERY_REMOVED', groceryId: grocery.id});
     } catch(err){
       dispatch({type: 'SET_ERROR', error: err});
       console.log(`Error: ${err.name} ${err.message}`);
     } 
   }
 
+  async function handleEdit() {
+    if(!groceryName)
+      return;
+
+    try{
+      await axios.patch(`${URI}/${grocery.id}`, {name: groceryName});
+      const updatedGrocery = {...grocery, name: groceryName}
+      dispatch({type: 'GROCERY_EDITED', updatedGrocery})
+    }catch(err){
+      dispatch({type: 'SET_ERROR', error: err});
+      console.log(`Error: ${err.name} ${err.message}`);
+    }finally{
+      setEditMode(!editMode);
+    }
+  }
+
   return (
     <li className="bg-white mb-4 mx-2 py-4 px-4 shadow-md text-lg rounded-lg flex justify-between">
-      <span className="text-gray-400">{grocery.name}</span>
+      {!editMode ? <span className="text-gray-400">{grocery.name}</span> :
+      <span><input className="bg-skin-button-accent" value={groceryName} onChange={(e) => setGroceryName(e.target.value)} type="text" name={grocery.id} id={grocery.id} /></span>}
       <span>
-        <span className='mr-2'><FontAwesomeIcon className="text-skin-accent" icon={faPenToSquare} /></span>
-        <span onClick={() => handleRemove(grocery.id)}><FontAwesomeIcon className="hover:text-red-900 text-red-600 hover:cursor-pointer active:text-green-600" icon={faTrashCan} /></span>
+        <span onClick={() => editMode ? handleEdit() : setEditMode(!editMode)} className='mr-2'><FontAwesomeIcon className={editMode ? 'text-skin-action' : 'text-skin-accent'} icon={faPenToSquare} /></span>
+        <span onClick={handleRemove}><FontAwesomeIcon className="hover:text-red-900 text-red-600 hover:cursor-pointer active:text-green-600" icon={faTrashCan} /></span>
       </span>
     </li>
   )
--- src/context/GroceryContextProvider.jsx
+++ src/context/GroceryContextProvider.jsx
@@ -14,6 +14,10 @@ function reducer(state, action) {
       const groceries = state.groceries.filter(grocery => grocery.id !== action.groceryId);
       return {...state, groceries};
     }
+    case 'GROCERY_EDITED': {
+      const groceries = state.groceries.map(grocery => grocery.id === action.updatedGrocery.id ? action.updatedGrocery : grocery);
+      return {...state, groceries};
+    }
     case 'SET_LOADING_STATUS':
       return {...state, loading: true, error: null};
     case 'CURRENT_GROCERY':
