10b42966977406e819ab4c54b6e247ed9f507385
John Lundgren <john.lundgren.lexicon@gmail.com>
2024-11-05 09:53:00 +0100

    Using useRef to add focus to inputfield on edit mode.

--- src/components/groceryItem/GroceryItem.jsx
+++ src/components/groceryItem/GroceryItem.jsx
@@ -2,13 +2,19 @@ import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPenToSquare, faTrashCan } from '@fortawesome/free-solid-svg-icons'
 import axios from 'axios';
-import {useContext, useState} from 'react';
+import {useContext, useState, useRef, useEffect} from 'react';
 import GroceryContext from '../../context/GroceryContext';
 
 const GroceryItem = ({grocery}) => {
   const {dispatch, URI} = useContext(GroceryContext);
   const [groceryName, setGroceryName] = useState(grocery.name);
   const [editMode, setEditMode] = useState(false);
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    if(editMode && inputRef.current)
+      inputRef.current.focus();
+  }, [editMode]);
 
   async function handleRemove() {
     try {
@@ -39,9 +45,9 @@ const GroceryItem = ({grocery}) => {
   return (
     <li className="bg-white mb-4 mx-2 py-4 px-4 shadow-md text-lg rounded-lg flex justify-between">
       {!editMode ? <span className="text-gray-400">{grocery.name}</span> :
-      <span><input className="bg-skin-button-accent" value={groceryName} onChange={(e) => setGroceryName(e.target.value)} type="text" name={grocery.id} id={grocery.id} /></span>}
+      <span><input ref={inputRef} className="bg-gray-100 text-green-600" value={groceryName} onChange={(e) => setGroceryName(e.target.value)} type="text" name={grocery.id} id={grocery.id} /></span>}
       <span>
-        <span onClick={() => editMode ? handleEdit() : setEditMode(!editMode)} className='mr-2'><FontAwesomeIcon className={editMode ? 'text-skin-action' : 'text-skin-accent'} icon={faPenToSquare} /></span>
+        <span onClick={() => editMode ? handleEdit() : setEditMode(true)} className='mr-2'><FontAwesomeIcon className={editMode ? 'text-skin-action' : 'text-skin-accent'} icon={faPenToSquare} /></span>
         <span onClick={handleRemove}><FontAwesomeIcon className="hover:text-red-900 text-red-600 hover:cursor-pointer active:text-green-600" icon={faTrashCan} /></span>
       </span>
     </li>
