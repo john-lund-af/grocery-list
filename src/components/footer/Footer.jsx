8c667e3621b23bc58c4855f65b0ac174c9a9ed25
John Lundgren <john.lundgren.lexicon@gmail.com>
2024-12-14 10:13:04 +0100

    Added some transparency to header and footer.

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,7 +2,7 @@ import InputField from '../inputField/InputField';
 
 const Footer = () => {
   return (
-    <div className='bg-skin-fill dark:bg-skin-dark-fill fixed bottom-0 w-full p-4'>
+    <div className='opacity-90 bg-skin-fill dark:bg-skin-dark-fill fixed bottom-0 w-full p-4'>
       <InputField />
     </div>
   )
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
   const {isDark, setIsDark} = useContext(ThemeContext);
 
   return (
-    <header className="fixed flex flex-row justify-between items-center w-full px-4 py-6 bg-skin-fill z-50 dark:bg-skin-dark-fill text-skin-inverted dark:text-skin-dark-base">
+    <header className="opacity-90 fixed flex flex-row justify-between items-center w-full px-4 py-6 bg-skin-fill z-50 dark:bg-skin-dark-fill text-skin-inverted dark:text-skin-dark-base">
       <div className='text-5xl'>Groceries</div>
       <div className="cursor-pointer">
         {isDark ? <span onClick={() => setIsDark(false)}><FontAwesomeIcon icon={faMoon} size="xl" /></span> :
